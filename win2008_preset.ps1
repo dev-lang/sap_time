dc890c6a26fd7977afadadced522778489b207c4
Franco Perez <56205122+dev-lang@users.noreply.github.com>
2023-10-15 10:34:33 -0300

    Create win2008_preset_machine.ps1
    se agrega script para cambiar computername, ver extensiones de archivos, cambiar pagefile, desactivar uac y reiniciar para aplicar todos los cambios en win2008

---
+++ win2008_preset.ps1
@@ -0,0 +1,24 @@
+# código adaptado para windows server 2008
+# ComputerName
+$ComputerName = "sapsrv"
+Rename-Computer -NewName $ComputerName -Force
+
+# Mostrar extensiones de archivo
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+
+# Memoria Virtual (swap)
+$PageFileSizeMB = 20500
+
+# Desactivar asignación automática
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "PagingFiles" -Value "C:\pagefile.sys $($PageFileSizeMB) $($PageFileSizeMB) 0 0"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "DisablePagingExecutive" -Value 1
+
+# Desactivar UAC
+# Ultimo paso previo a instalar EHP6
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$regName = "EnableLUA"
+$regValue = 0
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
+
+# Reiniciar para aplicar cambios
+Restart-Computer -Force
